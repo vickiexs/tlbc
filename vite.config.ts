d84333ab937c84c49614c675f731df1360fce3f4
mattcrewe <mattcrewe2308@hotmail.co.uk>
2024-02-09 19:06:32 +0000

    build config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tlbc/',
   plugins: [react()],
-  build: {
-    outDir: "./build",
-  },
   optimizeDeps: {
     exclude: ["portabletext_react"],
   },
