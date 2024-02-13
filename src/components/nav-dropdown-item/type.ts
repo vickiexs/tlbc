d661e9235e4ccef9fe596d8e8324f3e44e47ee11
vickiexs <victoriajsit@gmail.com>
2024-02-13 21:34:09 +0000

    fix(nav): fixed nav dropdown item props

--- src/components/nav-dropdown-item/type.ts
+++ src/components/nav-dropdown-item/type.ts
@@ -3,6 +3,6 @@ import { NavLinkProps as NavLink } from "../nav-link/type";
 export interface NavDropdownItemProps {
   label: string;
   dropdownItems: NavLink[];
-  variation?: string;
+  underlineColor?: string;
   closeMobileMenu: () => void;
 }
