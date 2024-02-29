840a68f839d1860b364b3bd8ca537eab7aa2dfa0
vickiexs <victoriajsit@gmail.com>
2024-02-29 16:12:24 +0000

    fix: updated image component type file

--- src/components/image/type.ts
+++ src/components/image/type.ts
@@ -1,4 +1,30 @@
-export interface LinkProps {
-  label: string;
-  link: string;
+export interface ImageProps {
+  _type: string;
+  asset: {
+    _id: string;
+    metadata: ImageMetadata;
+  };
+  crop: {
+    bottom: number;
+    left: number;
+    right: number;
+    top: number;
+    _type: string;
+  };
+  hotspot: {
+    height: number;
+    width: number;
+    x: number;
+    y: number;
+    _type: string;
+  };
+}
+
+export interface ImageMetadata {
+  lqip: string;
+  dimensions: {
+    aspectRatio: number;
+    height: number;
+    width: number;
+  };
 }
