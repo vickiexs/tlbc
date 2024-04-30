940b395ac7c6373725a7003a1db0ded8cc4dfa9d
vickiexs <victoriajsit@gmail.com>
2024-04-30 17:15:26 +0100

    fix: removed unused timeline folder

--- src/components/timeline/index.tsx
+++
@@ -1,8 +0,0 @@
-import Typography from "../typography";
-
-import { StyledContainer } from "./styled";
-// import { SectionProps } from "./type";
-
-export default function Timeline() {
-  return <StyledContainer>TIMELINE</StyledContainer>;
-}
