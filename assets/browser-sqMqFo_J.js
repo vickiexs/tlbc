b78ac83a2197ca4391fb25f9255e884d7268b1b4
vickiexs <victoriajsit@gmail.com>
2024-10-24 15:19:32 +0100

    Updates

--- assets/browser-sqMqFo_J.js
+++ assets/browser-sqMqFo_J.js
@@ -1,4 +1,4 @@
-import{c as Pe,g as Xe}from"./main-4F49zWNc.js";var se={exports:{}};/** @license
+import{c as Pe,g as Xe}from"./main-O4i_dAdk.js";var se={exports:{}};/** @license
  * eventsource.js
  * Available under MIT License (MIT)
  * https://github.com/Yaffle/EventSource/
